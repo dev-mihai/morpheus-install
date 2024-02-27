c621932dfb873daefb71d66d51ac5d9e0a631a40
dev-mihai <popa.mihail.n@gmail.com>
2024-02-26 23:05:07 -0700

    first commit

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    morpheus = {
+      source = "gomorpheus/morpheus"
+      version = "0.9.8"
+    }
+  }
+}
+
+provider "morpheus" {
+  url      = var.morpheus_url
+  access_token = var.access_token
+}
---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "morpheus_url" {
+    type         = string
+}
+
+variable "access_token" {
+    type         = string
+    sensitive    = true
+}
+
+variable "username" {
+  type    = string
+}
+
+variable "password" {
+  type    = string
+  sensitive = true
+}
+
+variable "licenseKey" {
+  type    = string
+  sensitive = true
+}
+
+variable "morph_version" {
+  type    = string
+  default = "6.2.7-2"
+}
